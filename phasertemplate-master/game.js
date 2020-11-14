0e2bed85d7a98bb6c4d42d5dc446df10b5eebe44
PabloFernandezAl01 <pablfe20@ucm.es>
2020-11-14 15:32:52 +0100

    Probando probando

--- phasertemplate-master/game.js
+++ phasertemplate-master/game.js
@@ -2,10 +2,12 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: "main" });
   }
-  preload() {}
-
+  preload() {
+    this.preload.sprite('favicon','phasertemplate-master/favicon.png');
+  }
   create() {
     this.add.text(10, 10, "Ivan es tonto pero Aida Pablo y Yojhan son preciosos", { fontColor: 0xffff00 });
+    this.add.sprite(100,200,'favicon');
   }
 
   update(time, delta) {}
