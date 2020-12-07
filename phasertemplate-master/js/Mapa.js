9df02e27b14448ff58606b8bf4f167d64f15d451
sryojhan <sryojhan@gmail.com>
2020-12-07 14:42:06 +0100

    corregido aun mas codigo

--- phasertemplate-master/js/Mapa.js
+++ phasertemplate-master/js/Mapa.js
@@ -67,38 +67,65 @@ export default class Mapa {
         let inicial = this.nodos[1][1];
 
         let final = this.nodos[3][10];
-        inicial.esPrincipio = true;
         final.esFin = true;
+
+        for (let i = 0; i < this.col; i++) {
+            for (let c = 0; c < this.fil; c++) {
+                this.nodos[i, c].cambiarDestino(final);
+            }
+        }
+
         let lista = [];
         this.addAdyancente(lista, inicial);
 
+        let resultado = this.recursiva(lista);
     }
-    addAdyancente(lista, nodoDestino) {
-        if (nodoDestino.x > 0 && !this.nodos[nodoDestino.x - 1][nodoDestino.y].visitada) {
-            lista.push(this.nodos[nodoDestino.x - 1][nodoDestino.y]);
+    addAdyancente(lista, nodoAct) {
+        if (nodoAct.x > 0) {
+            this.addNodo(lista, this.nodos[nodoAct.x - 1][nodoAct.y]);
         }
-        if (nodoDestino.x < this.col - 1 && !this.nodos[nodoDestino.x + 1][nodoDestino.y].visitada) {
-            lista.push(this.nodos[nodoDestino.x + 1][nodoDestino.y]);
+        if (nodoAct.x < this.col - 1) {
+            this.addNodo(lista, this.nodos[nodoAct.x + 1][nodoAct.y]);
+        }
+        if (nodoAct.y > 0) {
+            this.addNodo(lista, this.nodos[nodoAct.x][nodoAct.y - 1]);
+        }
+        if (nodoAct.y < this.fil - 1) {
+            this.addNodo(lista, this.nodos[nodoAct.x][nodoAct.y + 1]);
         }
 
-        if (nodoDestino.y > 0 && !this.nodos[nodoDestino.x][nodoDestino.y - 1].visitada) {
-            lista.push(this.nodos[nodoDestino.x][nodoDestino.y - 1]);
-        }
-        if (nodoDestino.y < this.fil - 1 && !this.nodos[nodoDestino.x][nodoDestino.y + 1].visitada) {
-            lista.push(this.nodos[nodoDestino.x][nodoDestino.y + 1]);
+    }
+    addNodo(lista, nodo) {
+        if (!nodo.visitada && !nodo.cellAct.ocupada) {
+            nodo.inicializar(nodoAct)
+            lista.push(nodo);
         }
     }
 
-    recursiva(lista, nodoAct) {
+    recursiva(lista) {
+        if (lista.length <= 0) return null;
 
+        let indice = 0;
+        let nodoAct = lista[0];
+        for (let i = 0; i < lista.length; i++) {
+            if (nodoAct.valor < lista[i].valor) {
+                nodoAct = lista[i];
+                indice = i;
+            }
+        }
 
+        if (nodoAct.esFin) return nodoAct;
+        nodoAct.visitada = true;
+        lista.splice(indice, 1);
 
+        this.addAdyancente(lista, nodoAct);
+
+        return this.recursiva(lista);
     }
 }
 class Nodo {
     constructor(celda) {
         this.esFin = false; //bool
-        this.esPrincipio = false; //bool
         this.visitada = false; //bool
         this.cellAct = celda; //celda
 
@@ -112,14 +139,19 @@ class Nodo {
         this.siguiente = null; //Nodo
         this.anterior = null; //Nodo
     }
-    inicializar(posx, posy, rec, nodoDestino, nodoAnterior = null) {
-        this.visitada = true; //bool
-        this.x = posx; //int
-        this.y = posy; //int
-        this.distanciaRecorrida = rec; //int
-        this.distanciaHastaElFinal = this.distanciaNodos(nodoDestino); //int
+    cambiarDestino(nodoDestino) {
+        this.visitada = false;
+        this.distanciaHastaElFinal = this.distanciaNodos(nodoDestino);
+        this.valor = this.distanciaRecorrida + this.distanciaHastaElFinal;
+    }
+    inicializar(nodoAnterior = null) {
+        if (nodoAnterior != null) {
+            this.distanciaRecorrida = nodoAnterior.distanciaRecorrida; //int
+        }
+        else {
+            this.distanciaRecorrida = 0;
+        }
         this.valor = this.distanciaRecorrida + this.distanciaHastaElFinal; //int
-
         this.anterior = nodoAnterior;
     }
     distanciaNodos(otro) {
