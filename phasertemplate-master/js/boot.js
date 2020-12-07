30b153c588d0e3ff884c9a6f3544523c219bf07d
ivasan07 <ivasan07@ucm.es>
2020-12-07 17:23:47 +0100

    intentando arreglar error de la página

--- phasertemplate-master/js/boot.js
+++ phasertemplate-master/js/boot.js
@@ -4,16 +4,16 @@ export default class Boot extends Phaser.Scene {
   }
   preload() {
     //Juego
-    this.load.image('jugador', "phasertemplate-master/images/Rey/ReyFrente.png");
-    this.load.image('sabana', "phasertemplate-master/images/Sabana.png");
+    this.load.image('jugador', "./phasertemplate-master/images/Rey/ReyFrente.png");
+    this.load.image('sabana', "./phasertemplate-master/images/Sabana.png");
 
-    this.load.image('ajustes',"phasertemplate-master/images/RuedaAjustes.PNG")
-    this.load.image('recursos', "phasertemplate-master/images/Recursos.png");
-    this.load.image('proxAtaque', "phasertemplate-master/images/proxAtaque.png");
-    this.load.image('desplegable', "phasertemplate-master/images/gestion.png");
-    this.load.image('construccion', "phasertemplate-master/images/gestion.png");
+    this.load.image('ajustes',"./phasertemplate-master/images/RuedaAjustes.PNG")
+    this.load.image('recursos', "./phasertemplate-master/images/Recursos.png");
+    this.load.image('proxAtaque', "./phasertemplate-master/images/proxAtaque.png");
+    this.load.image('desplegable', "./hasertemplate-master/images/gestion.png");
+    this.load.image('construccion', "./hasertemplate-master/images/gestion.png");
 
-    this.load.image('volveraljuego',"phasertemplate-master/images/gestion.png");
+    this.load.image('volveraljuego',"./phasertemplate-master/images/gestion.png");
   }
 
   create() {
