e4d05afacedd65d05b4081e847b4d4ebb46fe701
ivasan07 <ivasan07@ucm.es>
2020-12-26 17:55:07 +0100

    Trampa de suelo
    va en la misma clase junto a la de osos ya que la funcionalidad es casi igual

--- phasertemplate-master/js/trampaOsos.js
+++ phasertemplate-master/js/trampaOsos.js
@@ -1,6 +1,6 @@
 import EdificioDefensivo from "./edificioDefensivo.js";
 
-export default class TrampaOsos extends EdificioDefensivo {
+export default class Trampa extends EdificioDefensivo {
     constructor(scene, especialidad, vida, coste, posicion, ancho, alto, aldeanosMax, rango, key) {
         super(scene, especialidad, vida, coste, posicion, ancho, alto, aldeanosMax, rango, key);
         scene.physics.add.overlap(this, /*enemy*/, (trap, enemy) => {
