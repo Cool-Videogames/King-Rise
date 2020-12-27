4bd95f8abfea2fb7dd44050441d4a9d2a48cc6c4
ivasan07 <ivasan07@ucm.es>
2020-12-27 14:33:16 +0100

    Implementada Torre de arqueros

--- phasertemplate-master/js/trampa.js
+++ phasertemplate-master/js/trampa.js
@@ -3,10 +3,13 @@ import EdificioDefensivo from "./edificioDefensivo.js";
 export default class Trampa extends EdificioDefensivo {
     constructor(scene, especialidad, vida, coste, posicion, ancho, alto, aldeanosMax, rango, key) {
         super(scene, especialidad, vida, coste, posicion, ancho, alto, aldeanosMax, rango, key);
-        scene.physics.add.overlap(this, /*enemy*/, (trap, enemy) => {
+        scene.physics.add.overlap(this, /*enemy*/ (trap, enemy) => {
             if (especialidad === 'trampaSuelo')
                 enemy.destroy();
-            else this.atacar(enemy, 50);
+            else{
+                 this.atacar(enemy, 50);
+                 //STUNEAR ENEMIGO
+                }
             trap.destroy();
         })
     }
