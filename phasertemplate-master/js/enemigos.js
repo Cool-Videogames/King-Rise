5487b870e9cc90010101d2171b9fd8634201a061
sryojhan <sryojhan@gmail.com>
2020-12-31 23:26:13 +0100

    Comenzado enemigo

---
+++ phasertemplate-master/js/enemigos.js
@@ -0,0 +1,14 @@
+import Persona from "./persona";
+
+import * as config from "./config.js"
+import Persona from "./persona.js"
+
+export default class Enemigo extends Persona{
+    constructor(scene, pos, vida, fuerza, key){
+        super(scene, pos, vida, fuerza, key);
+
+        
+
+
+    }
+}
